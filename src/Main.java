ed8d2da3aa5a305bbac437aef0803fd677bb2246
VICIWUOHA <viciwuoha@gmail.com>
2023-03-12 18:18:54 +0100

    Updated Main Script

--- src/Main.java
+++ src/Main.java
@@ -8,12 +8,5 @@ public static void main(String[] args) throws IOException, InterruptedException 
         String transformedFilePath = EltTransformer.transformData(rawFilePath);
         EltTransformer.loadCsvToBigQuery("sales", "Products", transformedFilePath);
 
-//    String runTime = DataTransformer.getRuntime();
-//    DataTransformer EltTransformer = new DataTransformer();
-//    String rawFilePath = DataTransformer.getAndLoadData("https://fakestoreapi.com/products");
-//    String transformedFilePath = DataTransformer.transformData(rawFilePath,runTime);
-//    DataTransformer.loadCsvToBigQuery("sales",
-
-
 }
 }
